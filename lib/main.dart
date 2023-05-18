1845b0f2fef8e3c933fceb9ab99ff95e351af53f
tctr <thierry.coutier@gmail.com>
2023-05-18 14:32:16 +0200

    Adding a choice between two pages to be displayed

--- lib/main.dart
+++ lib/main.dart
@@ -53,6 +53,18 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = Placeholder();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
     return Scaffold(
       // The new MyHomePage contains a Row with two children. The first widget is SafeArea, and the second
       // is an Expanded widget.
@@ -93,7 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
           Expanded(
             child: Container(
               color: Theme.of(context).colorScheme.primaryContainer,
-              child: GeneratorPage(),
+              child: page,
             ),
           ),
         ],
